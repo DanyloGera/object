36a6a3fef8952c12607a17dfe712dede10ae940b
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2020-09-17 19:04:03 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var car = {
+  name: "Lamborghini",
+  model: "Lamborghini Veneno",
+  speed: 350,
+}
+
+  console.log(String("name:" + car.name));
+  console.log(String("model:" + car.model));
+  console.log(String("speed:" + car.speed));
+
+
+  console.log(Boolean("name:" + car.name));
+  console.log(Boolean("model:" + car.model));
+  console.log(Boolean("speed:" + car.speed));
+
+  console.log(Number(car.num));
+  console.log(Number(car.model));
+  console.log(Number(car.speed));
+
+
+  if (car.name = car.name)
+  console.log("True!");
+else if(car.name = car.model)
+  console.log("False!");
+else if (car.name = car.speed)
+  console.log("False!");
+else if(car.model = car.name)
+  console.log("False!");
+else if(car.model = car.model)
+  console.log("True!");
+else if (car.model = car.speed)
+  console.log("False!");
+else if(car.speed = car.name)
+  console.log("False!");
+else if(car.speed = car.model)
+  console.log("False!");
+else if (car.speed = car.speed)
+  console.log("True");
